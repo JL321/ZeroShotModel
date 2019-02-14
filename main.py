6da3f6dcc0c6fce8766aae8ebe976e82771e027f
JL321 <james.liangyy@gmail.com>
2019-02-13 22:56:45 -0500

    *update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import tensorflow as tf
-from Preprocessing import save_expanded, encode_oneHot, load_embeddings
+from Preprocessing import save_expanded, encode_oneHot, load_embeddings, convert_format
 import tensorflow.keras.datasets as datasets  
 from model import zsModel
 import matplotlib.pyplot as plt
@@ -23,19 +23,39 @@ def main():
                                           output_shape = (None, 10),
                                           feature_shape = 100,
                                           batch_size = 16,
-                                          filters = [[64,3, True],
-                                                     [128,3, False],
-                                                     [128,3, True],
-                                                     [128,3, False],
-                                                     [256,3, False],
+                                          filters = [[64,3, True, 'SAME'],
+                                                     [128,3, False, 'SAME'],
+                                                     [128,3, False, 'SAME'],
+                                                     [128,3, False, 'VALID'],
+                                                     [256,3, True, 'VALID'],
+                                                     [256,3, False, 'VALID']
                                                      ])
     
     newModel = zsModel(hparams, "ZSModel")
+    #newModel.load_weights("newModel.ckpt")
     print("GRAPH BUILT")
-    loss = newModel.train(x_train, y_train, 4)
+
+    loss = newModel.train(x_train, y_train, 2)
     plt.plot(loss)
     plt.ylabel("Loss (Accumulation per Epoch)")
     plt.xlabel("Step")
+    plt.show()
+    
+    newModel.accuracy_test(x_train, y_train)
+    
+    imgList = convert_format("zsImages")
+    for img in imgList:
+        euc_distances = []
+        features = newModel.featurePredicton(np.expand_dims(img, axis = 0))
+        for test_class in zsl_dict.keys():
+            euc_distances.append(np.linalg.norm(features-zsl_dict[test_class]))
+        min_idx = np.argmin(np.array(euc_distances))
+        for i,v in enumerate(zsl_dict.keys()):
+            if (i == min_idx):
+                print("Prediction: {}".format(v))
+        plt.imshow(img)
+        plt.show()
+    
     
 if __name__ == "__main__":
     main()
